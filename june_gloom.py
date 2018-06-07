337bbdc8df875e839c6b363a18a0b3767004add1
samcotten <samcotten@gmail.com>
2018-06-07 08:56:48 -0700

    split analyses into separate files, expanded NOAA analysis

--- june_gloom.py
+++ june_gloom.py
@@ -1,150 +1,57 @@
 #!/usr/bin/env python
-from datetime import datetime, timedelta
-import time
-from collections import namedtuple
-import requests
 import pandas as pd
 
+# Static Vars
 data_dir = r"raw_data/"
-combined_weather_file = "combined_weather_data.csv"
 daily_noaa_file = "CDO4274757652424.txt"
 enso_sst_file = "sst_anomaly_3month_mean.csv"
-
-month_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-month_dict = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 
-              7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
-enso_dict = {
-    1973: 'el nino', 1978: 'el nino', 1980: 'el nino', 1983: 'el nino', 1987: 'el nino',
-    1988: 'el nino', 1992: 'el nino', 1995: 'el nino', 1998: 'el nino', 2003: 'el nino',
-    2007: 'el nino', 2010: 'el nino', 1971: 'la nina', 1974: 'la nina', 1976: 'la nina',
-    1989: 'la nina', 1999: 'la nina', 2000: 'la nina', 2008: 'la nina', 2011: 'la nina',
-    2012: 'la nina'
-}
+dict_3mo_mean = {1: 'DJF', 2: 'JFM', 3: 'FMA', 4: 'MAM', 5: 'AMJ', 6: 'MJJ',
+                 7: 'JJA', 8: 'JAS', 9: 'ASO', 10: 'SON', 11: 'OND', 12: 'NDJ'}
 dict_enso_sst = {}
-
-combined_weather_df = pd.DataFrame()
 df_noaa = pd.DataFrame()
 df_enso_sst = pd.DataFrame()
 
 
-api_key = '8940b5f3356ab273'
-base_url = "http://api.wunderground.com/api/{}/history_{}/q/CA/ksmo.json"
-records = []
-
-
 def init():
-    global combined_weather_df, df_noaa, df_enso_sst
-    combined_weather_df = pd.read_csv(data_dir + combined_weather_file)
-#    combined_noaa_sst_df = pd.read_csv(data_dir + )
+    global df_noaa, df_enso_sst
     df_noaa = pd.read_csv(data_dir + daily_noaa_file)
     df_enso_sst = pd.read_csv(data_dir + enso_sst_file)
 
 
 def noaa_clean():
-    global df_noaa, df_lax, enso_dict
-    df_lax = df_noaa[df_noaa['STN---'] == 722950]
+    global df_noaa, df_lax
+    df_lax = df_noaa.loc[df_noaa['STN---'] == 722950].copy()
     df_lax.reset_index(inplace=True)
     df_lax.drop(['index', 'STN---', 'WBAN ', '  ', '  .1', '  .2', '  .3', '  .4', '  .5', 'Unnamed: 22'], axis=1,
                 inplace=True)
-    df_lax[' YEARMODA'] = pd.to_datetime(df_lax[' YEARMODA'], format='%Y%m%d')
-    df_lax['month'] = df_lax[' YEARMODA'].apply(lambda date: date.month)
-    df_lax['year'] = df_lax[' YEARMODA'].apply(lambda date: date.year)
-    df_lax['enso'] = df_lax['year'].map(enso_dict)
-    df_lax['enso'].fillna('neutral', inplace=True)
+    df_lax.columns = df_lax.columns.str.strip()
+    df_lax['YEARMODA'] = pd.to_datetime(df_lax['YEARMODA'], format='%Y%m%d')
+    df_lax['month'] = df_lax['YEARMODA'].apply(lambda date: date.month)
+    df_lax['year'] = df_lax['YEARMODA'].apply(lambda date: date.year)
+    df_lax['MAX'] = df_lax['MAX'].apply(lambda col: float(col.strip('*')))
+    df_lax['MIN'] = df_lax['MIN'].apply(lambda col: float(col.strip('*')))
+    df_lax['PRCP'] = df_lax['PRCP'].apply(lambda col: float(col[:-1].strip()))
+    df_lax['PRCP'] = df_lax['PRCP'].apply(impute_null_prcp)
+    df_lax['dummy'] = 1
 
 
-def weather_data_features():
-    global combined_weather_df, pt_june_gloom_df, fog_days_df, rain_days_df, pt_temp_df, min_max_temp_df, \
-        pt_fog_by_month, year_min, year_max
-    combined_weather_df['date'] = pd.to_datetime(combined_weather_df['date'])
-    combined_weather_df['year'] = combined_weather_df['date'].apply(lambda date: date.year)
-    combined_weather_df['month'] = combined_weather_df['date'].apply(lambda date: date.month)
-    combined_weather_df['day'] = combined_weather_df['date'].apply(lambda date: date.day)
-    combined_weather_df['dayofweek'] = combined_weather_df['date'].apply(lambda date: date.dayofweek)
-    combined_weather_df.drop('Unnamed: 0', axis=1, inplace=True)
-    year_min = combined_weather_df['year'].unique().min()
-    year_max = combined_weather_df['year'].unique().max()
-    pt_june_gloom_df = combined_weather_df.pivot_table(index='month', columns=['day', 'year'], values='fog')
-    fog_days_df = combined_weather_df[(combined_weather_df['fog'] == 1) & (combined_weather_df['rain'] == 0)][
-        ['month', 'year', 'meanwdird', 'meantempm', 'meandewptm', 'meanpressurem', 'fog']]
-    fog_days_df.set_index('month', inplace=True)
-    rain_days_df = combined_weather_df[combined_weather_df['rain'] == 1][
-        ['month', 'year', 'meanwdird', 'meantempm', 'meandewptm', 'meanpressurem']]
-    rain_days_df.set_index('month', inplace=True)
-    pt_temp_df = combined_weather_df.pivot_table(index='month', columns=['day', 'year'],
-                                                 values=['meantempm'])
-    min_max_temp_df = combined_weather_df[['date', 'year', 'mintempm', 'maxtempm']]
-    min_max_temp_df['month day'] = min_max_temp_df['date'].apply(lambda date:
-                                                                 (date.month, date.day))
-    fog_days_df.reset_index(inplace=True)
-    pt_fog_by_month = fog_days_df.pivot_table(values='fog', index='month',
-                                              columns=['year'], aggfunc='sum', fill_value=0)
-    pt_fog_by_month = pt_fog_by_month.unstack(level='year')
-
-
-def wug_data_concat():
-    global data_dir
-    file_01 = pd.read_csv(data_dir + "1997")
-    file_02 = pd.read_csv(data_dir + "1998")
-    file_03 = pd.read_csv(data_dir + "1999")
-    file_04 = pd.read_csv(data_dir + "2000")
-    file_05 = pd.read_csv(data_dir + "2001")
-    file_06 = pd.read_csv(data_dir + "2002")
-    file_07 = pd.read_csv(data_dir + "2003")
-    file_08 = pd.read_csv(data_dir + "2004")
-    file_09 = pd.read_csv(data_dir + "2005")
-    file_10 = pd.read_csv(data_dir + "2006")
-    file_11 = pd.read_csv(data_dir + "2007")
-    file_12 = pd.read_csv(data_dir + "2008")
-    file_13 = pd.read_csv(data_dir + "2009")
-    file_14 = pd.read_csv(data_dir + "2010")
-    file_15 = pd.read_csv(data_dir + "2011")
-    file_16 = pd.read_csv(data_dir + "2012")
-    concat_weather_df = pd.concat([file_01, file_02, file_03, file_04, file_05, file_06, file_07, file_08, file_09,
-                                   file_10, file_11, file_12, file_13, file_14, file_15, file_16])
-    concat_weather_df.to_csv(data_dir + combined_weather_file)
-
-
-def wug_api_request(year):
-    global data_dir, target_date
-    target_date = datetime(year, 1, 1)
-    if year % 4 == 0:
-        leap = 366
+def impute_null_prcp(col):
+    if col == 99.99:
+        return 0.0
     else:
-        leap = 365
-    features = ["date", "fog", "rain", "meanwdird", "meanwindspdm", "meantempm", "meandewptm", "meanpressurem",
-                "maxhumidity", "minhumidity", "maxtempm", "mintempm", "maxdewptm", "mindewptm",
-                "maxpressurem", "minpressurem", "precipm"]
-    DailySummary = namedtuple("DailySummary", features)
+        return col
 
-    def extract_weather_data(days):
-        global records, api_key, base_url, target_date
-        for _ in range(days):
-            request = base_url.format(api_key, target_date.strftime('%Y%m%d'))
-            response = requests.get(request)
-            if response.status_code == 200:
-                data = response.json()['history']['dailysummary'][0]
-                records.append(DailySummary(
-                    date=target_date,
-                    fog=data['fog'],
-                    rain=data['rain'],
-                    meanwdird=data['meanwdird'],
-                    meanwindspdm=data['meanwindspdm'],
-                    meantempm=data['meantempm'],
-                    meandewptm=data['meandewptm'],
-                    meanpressurem=data['meanpressurem'],
-                    maxhumidity=data['maxhumidity'],
-                    minhumidity=data['minhumidity'],
-                    maxtempm=data['maxtempm'],
-                    mintempm=data['mintempm'],
-                    maxdewptm=data['maxdewptm'],
-                    mindewptm=data['mindewptm'],
-                    maxpressurem=data['maxpressurem'],
-                    minpressurem=data['minpressurem'],
-                    precipm=data['precipm']))
-            time.sleep(6)
-            target_date += timedelta(days=1)
-        return records
-    records = extract_weather_data(leap)
-    r = pd.DataFrame(records)
-    r.to_csv(data_dir + '{}'.format(target_date.year), index=False)
+
+def feature_sst_anomaly():
+    global df_lax, df_enso_sst, dict_enso_sst
+    df_enso_sst.set_index('Year', inplace=True)
+    dict_enso_sst = df_enso_sst.to_dict()
+    df_lax['3mo_mean'] = df_lax['month'].map(dict_3mo_mean)
+    df_lax['sst_anomaly'] = df_lax[['3mo_mean', 'year']].apply(sst_anomaly_lookup, axis=1)
+    df_lax.drop('3mo_mean', axis=1, inplace=True)
+
+
+def sst_anomaly_lookup(cols):
+    mean = cols[0]
+    year = cols[1]
+    return dict_enso_sst[mean][year]
---
+++ wug_api_data.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+from datetime import datetime, timedelta
+import time
+from collections import namedtuple
+import requests
+import pandas as pd
+
+# Static Vars
+data_dir = r"raw_data/"
+combined_weather_file = "combined_weather_data.csv"
+enso_sst_file = "sst_anomaly_3month_mean.csv"
+month_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+month_dict = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun',
+              7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
+combined_weather_df = pd.DataFrame()
+df_enso_sst = pd.DataFrame()
+api_key = '8940b5f3356ab273'
+base_url = "http://api.wunderground.com/api/{}/history_{}/q/CA/ksmo.json"
+records = []
+
+
+def init():
+    global combined_weather_df, df_enso_sst
+    combined_weather_df = pd.read_csv(data_dir + combined_weather_file)
+    df_enso_sst = pd.read_csv(data_dir + enso_sst_file)
+
+
+def wug_data_clean():
+    global combined_weather_df
+    combined_weather_df['date'] = pd.to_datetime(combined_weather_df['date'])
+    combined_weather_df['year'] = combined_weather_df['date'].apply(lambda date: date.year)
+    combined_weather_df['month'] = combined_weather_df['date'].apply(lambda date: date.month)
+    combined_weather_df['day'] = combined_weather_df['date'].apply(lambda date: date.day)
+    combined_weather_df['dayofweek'] = combined_weather_df['date'].apply(lambda date: date.dayofweek)
+    combined_weather_df.drop('Unnamed: 0', axis=1, inplace=True)
+    combined_weather_df['precipm'] = combined_weather_df['precipm'].apply(impute_trace_precip)
+    combined_weather_df['precipm'] = combined_weather_df['precipm'].apply(lambda col: float(col))
+
+
+def wug_data_features():
+    global combined_weather_df, pt_june_gloom_df, fog_days_df, rain_days_df, pt_temp_df, min_max_temp_df, \
+        pt_fog_by_month, year_min, year_max
+    year_min = combined_weather_df['year'].unique().min()
+    year_max = combined_weather_df['year'].unique().max()
+    pt_june_gloom_df = combined_weather_df.pivot_table(index='month', columns=['day', 'year'], values='fog')
+    fog_days_df = combined_weather_df[(combined_weather_df['fog'] == 1) & (combined_weather_df['rain'] == 0)][
+        ['month', 'year', 'meanwdird', 'meantempm', 'meandewptm', 'meanpressurem', 'fog']]
+    fog_days_df.set_index('month', inplace=True)
+    rain_days_df = combined_weather_df[combined_weather_df['rain'] == 1][['month', 'year', 'meanwdird', 'meantempm',
+                                                                          'meandewptm', 'meanpressurem']]
+    rain_days_df.set_index('month', inplace=True)
+    pt_temp_df = combined_weather_df.pivot_table(index='month', columns=['day', 'year'],
+                                                 values=['meantempm'])
+    min_max_temp_df = combined_weather_df[['date', 'year', 'mintempm', 'maxtempm']].copy()
+    min_max_temp_df['month day'] = min_max_temp_df['date'].apply(lambda date: (date.month, date.day))
+    fog_days_df.reset_index(inplace=True)
+    pt_fog_by_month = fog_days_df.pivot_table(values='fog', index='month', columns=['year'], aggfunc='sum',
+                                              fill_value=0)
+    pt_fog_by_month = pt_fog_by_month.unstack(level='year')
+
+
+def impute_trace_precip(col):
+    if col == 'T':
+        return 0.25
+    else:
+        return col
+
+
+def wug_data_concat():
+    global data_dir
+    file_01 = pd.read_csv(data_dir + "1997")
+    file_02 = pd.read_csv(data_dir + "1998")
+    file_03 = pd.read_csv(data_dir + "1999")
+    file_04 = pd.read_csv(data_dir + "2000")
+    file_05 = pd.read_csv(data_dir + "2001")
+    file_06 = pd.read_csv(data_dir + "2002")
+    file_07 = pd.read_csv(data_dir + "2003")
+    file_08 = pd.read_csv(data_dir + "2004")
+    file_09 = pd.read_csv(data_dir + "2005")
+    file_10 = pd.read_csv(data_dir + "2006")
+    file_11 = pd.read_csv(data_dir + "2007")
+    file_12 = pd.read_csv(data_dir + "2008")
+    file_13 = pd.read_csv(data_dir + "2009")
+    file_14 = pd.read_csv(data_dir + "2010")
+    file_15 = pd.read_csv(data_dir + "2011")
+    file_16 = pd.read_csv(data_dir + "2012")
+    concat_weather_df = pd.concat([file_01, file_02, file_03, file_04, file_05, file_06, file_07, file_08, file_09,
+                                   file_10, file_11, file_12, file_13, file_14, file_15, file_16])
+    concat_weather_df.to_csv(data_dir + combined_weather_file)
+
+
+def wug_api_request(year):
+    global data_dir, target_date
+    target_date = datetime(year, 1, 1)
+    if year % 4 == 0:
+        leap = 366
+    else:
+        leap = 365
+    features = ["date", "fog", "rain", "meanwdird", "meanwindspdm", "meantempm", "meandewptm", "meanpressurem",
+                "maxhumidity", "minhumidity", "maxtempm", "mintempm", "maxdewptm", "mindewptm",
+                "maxpressurem", "minpressurem", "precipm"]
+    DailySummary = namedtuple("DailySummary", features)
+
+    def extract_weather_data(days):
+        global records, api_key, base_url, target_date
+        for _ in range(days):
+            request = base_url.format(api_key, target_date.strftime('%Y%m%d'))
+            response = requests.get(request)
+            if response.status_code == 200:
+                data = response.json()['history']['dailysummary'][0]
+                records.append(DailySummary(
+                    date=target_date,
+                    fog=data['fog'],
+                    rain=data['rain'],
+                    meanwdird=data['meanwdird'],
+                    meanwindspdm=data['meanwindspdm'],
+                    meantempm=data['meantempm'],
+                    meandewptm=data['meandewptm'],
+                    meanpressurem=data['meanpressurem'],
+                    maxhumidity=data['maxhumidity'],
+                    minhumidity=data['minhumidity'],
+                    maxtempm=data['maxtempm'],
+                    mintempm=data['mintempm'],
+                    maxdewptm=data['maxdewptm'],
+                    mindewptm=data['mindewptm'],
+                    maxpressurem=data['maxpressurem'],
+                    minpressurem=data['minpressurem'],
+                    precipm=data['precipm']))
+            time.sleep(6)
+            target_date += timedelta(days=1)
+        return records
+    records = extract_weather_data(leap)
+    r = pd.DataFrame(records)
+    r.to_csv(data_dir + '{}'.format(target_date.year), index=False)
