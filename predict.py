cc05286e36a807e456453df2b58f2c4adcc88815
Sam Cotten <samcotten@gmail.com>
2018-09-10 19:24:06 -0700

    update predictions to include both dtc, gbc

--- predict.py
+++ predict.py
@@ -3,16 +3,21 @@ from sklearn.externals import joblib
 
 
 def predict():
+    results = {}
+    models = ["dtree_model.pkl", "gbc_model.pkl"]
     input_features = request_conditions.main()
-    clf = joblib.load("gbc_model.pkl")
-    pred = clf.predict(input_features)
+    for model in models:
+        clf = joblib.load(model)
+        pred = clf.predict(input_features)
+        results[model] = pred
 
-    return pred
+    return results
 
 
 if __name__ == "__main__":
     prediction = predict()
-    if prediction == [1]:
-        print("The fog should roll in today!")
-    elif prediction == [0]:
-        print("Clear skies this evening!")
+    for model, results in prediction.items():
+        if results == [1]:
+            print("{} model says.. the fog should roll in tonight!".format(model))
+        elif results == [0]:
+            print("{} model says... clear skies this evening!".format(model))
